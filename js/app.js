e16f745d4fb981a3dc0d04a915f49528ad1d20df
Giulia Helena Gelman F Baracat <162526097+ggelman@users.noreply.github.com>
2024-09-18 21:16:47 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function comprar() {
     let qtdInput = document.getElementById('qtd');
     let quantidade = parseInt(document.getElementById('qtd').value);
 
-    if (qtd <= 0) {
+    if (quantidade <= 0) {
         alert('Por favor, insira uma quantidade válida.');
         return;
     }
